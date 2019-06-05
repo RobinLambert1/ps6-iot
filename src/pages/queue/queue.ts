9c24311810d9769a0964f3c2080878c589c0ad3a
ComeDemolliere <come.demolliere@etu.unice.fr>
2019-06-05 16:56:28 +0200

    add list viewer management #6

--- src/pages/queue/queue.ts
+++ src/pages/queue/queue.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import {Field} from "../../models/field";
 
 /**
  * Generated class for the QueuePage page.
@@ -15,11 +16,37 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 })
 export class QueuePage {
 
+  field: Field;
+  values: any[][];
+
   constructor(public navCtrl: NavController, public navParams: NavParams) {
   }
 
   ionViewDidLoad() {
-    console.log('ionViewDidLoad QueuePage');
+    this.values = [];
+    this.field = this.navParams.get("item");
+
+    for (let i = 0; i < this.field.queue.length; i++) {
+      const listValue = Object.keys(this.field.queue[i]).filter(x => {
+        if(x !== 'allElem'){
+          const display = this.field.queue[i][x]["visible"];
+          if(display !== undefined){
+            if(display === true) return true;
+          }
+        }
+      });
+      this.values.push(listValue);
+    }
+    console.log(this.values);
   }
 
+  drawItem(index: number, item: any){
+    if(this.field.queue[index][item]["display"] === undefined)
+      return this.field.queue[index][item]["value"];
+    return this.field.queue[index][item]["display"] + ': ' + this.field.queue[index][item]["value"];
+  }
+
+  drawColor(index: number){
+    return this.field.queue[index]["allElem"]["background_color"];
+  }
 }
