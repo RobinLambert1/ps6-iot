9be18da8d4d63968937e64bec1116e901bb83852
Louis-Amas <amaslouis@gmail.com>
2019-06-06 15:11:28 +0200

    added mqtt

---
+++ src/providers/mqtt/mqtt.ts
@@ -0,0 +1,28 @@
+import { Injectable } from '@angular/core';
+import {MqttService} from "ngx-mqtt";
+import {Field} from "../../models/field";
+
+/*
+  Generated class for the MqttProvider provider.
+
+  See https://angular.io/guide/dependency-injection for more info on providers
+  and Angular DI.
+*/
+@Injectable()
+export class MqttProvider {
+
+  constructor(private mqttService: MqttService) {
+
+  }
+
+  /**
+   * Need to have req.body defined to send value.
+   * @param req
+   */
+  manageMqtt(req: Field) {
+    if (req.mqtt === undefined) {
+      this.mqttService.publish(req.mqtt.request, req.body[req.mqtt.value])
+        .subscribe(() => console.log());
+    }
+  }
+}
